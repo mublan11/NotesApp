9bc9dd084a2adbfd24c54135f79bde84e20b73a2
Tu Nombre <diegomurillo@uaz.edu.mx>
2019-02-10 23:22:54 -0600

    Delete note function is added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { AddNotePage } from '../add-note/add-note';
 import { NoteService } from '../../providers/note-service/note-service' 
 import { Note } from '../../models/note.model';
+import { ViewNotePage } from '../view-note/view-note';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +11,8 @@ import { Note } from '../../models/note.model';
 })
 export class HomePage {
 
-  notes: Promise<Note[]>;
+  private notes: Promise<Note[]>;
+  private note: Note;
  
   constructor(public navCtrl: NavController,
     private noteService: NoteService) {
@@ -23,6 +25,14 @@ export class HomePage {
 
   addNote(){
     this.navCtrl.push(AddNotePage);
+  }
+
+  getNote(createDate: number){
+    this.noteService.getNote(createDate).then((n) => {
+      this.note = n;
+      this.navCtrl.push(ViewNotePage, { note: this.note })
+    });
+
   }
 
   getAllNotes(){
